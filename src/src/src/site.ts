92a645df832505cca1d092a9c9aed265f541ca7e
jeferb368-byte <jeferb368@gmail.com>
2026-09-04 00:46:42 -0500

    Create site.ts with site configuration

---
+++ src/src/src/site.ts
@@ -0,0 +1,5 @@
+export const SITE = {
+  name: 'TIENDA MAYOR',
+  whatsappNumber: '573114360726', // <-- REEMPLAZA ESTE NÚMERO CON EL TUYO (ejemplo: 573001234567)
+  metaPixelId: '', // Vacío por ahora
+}
